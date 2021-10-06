afeaa347366e7168b7c0363313d4608d1ab1b12f
milankatira <milankatira26@gmail.com>
2021-10-06 10:29:34 +0530

    initial commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,18 @@ const Header = () => {
     return (
         <Container>
             <a>
-            <img src="./images/logo.svg" alt="logo" />
+                <img src="./images/logo.svg" alt="logo" />
             </a>
+            <Menu>
+                <a href="#">model s</a>
+                <a href="#">model 3</a>
+                <a href="#">model x</a>
+                <a href="#">model y</a>
+            </Menu>
+            <RightMenu>
+                <a href="#">Shop</a>
+                <a href="#">Tesla account</a>
+            </RightMenu>
         </Container>
     )
 }
@@ -13,5 +23,35 @@ const Header = () => {
 export default Header
 
 const Container = styled.div`
+min-height:60px;
+position:fixed;
+display:flex;
+align-items:center;
+padding:0 20px;
+top:0;
+left:0;
+right:0;
+`
 
+const Menu = styled.div`
+display:flex;
+align-items:center;
+flex:1;
+justify-content:center;
+
+a{
+font-weight:600;
+text-transform:uppercase;
+padding:0 10px;
+flex-wrap:no-wrap;
+}
+`
+
+const RightMenu = styled.div`
+a{
+font-weight:600;
+text-transform:uppercase;
+margin-right:10px;
+flex-wrap:no-wrap;
+}
 `
